fdbbee08a7427adfe207d5e75dde04897e97519c
Angkita Paul <paulankita614@gmail.com>
2026-08-24 12:54:19 +0530

    Add Chat route to employeeRoutes

--- src/app/routes/employeeRoutes.jsx
+++ src/app/routes/employeeRoutes.jsx
@@ -1,3 +1,4 @@
+import Chat from "../../features/chats/ui/pages/Chat";
 import Attendance from "../../features/employee module/Attendance/ui/pages/Attendance";
 import MyTask from "../../features/employee module/MyTask/ui/pages/MyTask";
 import Profile from "../../features/employee module/profile/ui/pages/Profile";
@@ -7,7 +8,10 @@ export let employeeRoutes = [
     path: "/home/myTask",
     element: <MyTask />,
   },
-
+  {
+    path: "chat",
+    element: <Chat />,
+  },
   {
     path: "/home/attendance",
     element: <Attendance />,
