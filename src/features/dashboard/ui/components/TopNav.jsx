c2c470e5cc91841b5db1bdc482f22339f707ea2f
Angkita Paul <paulankita614@gmail.com>
2026-08-26 11:58:13 +0530

    Refactor TopNav and Home components

--- src/features/dashboard/ui/components/TopNav.jsx
+++ src/features/dashboard/ui/components/TopNav.jsx
@@ -4,15 +4,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../../../../shared/state/themeSlice";
 
 const TopNav = () => {
-  const { mode } = useSelector((store) => store.theme);
-  const dispatch = useDispatch();
+  let dispatch = useDispatch();
 
-  const handleThemeChange = () => {
+  let { mode } = useSelector((store) => store.theme);
+
+  let handleThemeChange = () => {
     dispatch(toggleTheme());
   };
 
   return (
-    <div className="flex justify-between bg-(--bg-card) items-center">
+    <div className="flex justify-between items-center">
       <div className="flex gap-4 items-center w-[30%] rounded px-3 py-2 bg-(--bg-surface) border border-gray-600">
         <Search size={23} />
         <input
--- src/features/dashboard/ui/pages/Home.jsx
+++ src/features/dashboard/ui/pages/Home.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { toggleTheme } from "../../../../shared/state/themeSlice";
 
 const Home = () => {
-  let dispatch = useDispatch();
-
-  let handleThemeChange = () => {
-    dispatch(toggleTheme());
-  };
-
   return (
     <div>
       <h1>This is dashboard Home Page</h1>
-      <button
-        onClick={handleThemeChange}
-        className="px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Change Theme
-      </button>
     </div>
   );
 };
